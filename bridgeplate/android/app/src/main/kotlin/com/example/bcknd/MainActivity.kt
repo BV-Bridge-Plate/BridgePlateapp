f942b4eba28690a56d506fbc984b3cff6126f694
Nitish G. Saji <nitishsaji2001@gmail.com>
2023-10-20 09:04:53 +0530

    draft1

---
+++ bridgeplate/android/app/src/main/kotlin/com/example/bcknd/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bcknd
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ bridgeplate/android/app/src/main/kotlin/com/example/bridgeplate/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bridgeplate
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
